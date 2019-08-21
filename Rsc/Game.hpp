4c81ec486cd1fc2a5cc38b2acc165014791ff522
Tom <31548102+HTomJMW@users.noreply.github.com>
2019-08-21 21:01:52 +0200

    Update Game.hpp

--- Rsc/Game.hpp
+++ Rsc/Game.hpp
@@ -4,11 +4,11 @@ respawnDelay = 10;
 respawnDialog = false;
 
 //--- Require briefing.html to show up.
-onLoadMission = "BECTI MALDEN - Zerty/HTom/Yoshi_E 1.3.5.0";
+onLoadMission = "BECTI MALDEN - Zerty/HTom/Yoshi_E 1.3.5.1";
 onLoadMissionTime = false;
 onLoadName = "BECTI MALDEN - Zerty/HTom/Yoshi_E";
 
-briefingName = "BECTI MALDEN - Zerty/HTom/Yoshi_E 1.3.5.0";
+briefingName = "BECTI MALDEN - Zerty/HTom/Yoshi_E 1.3.5.1";
 
 //--- ArmA 3 Specifics
 dev = "Benny";
