3d99710a3e798871041b37a52e87a0922c8b6e44
Tom <31548102+HTomJMW@users.noreply.github.com>
2019-10-12 15:27:04 +0200

    Update Parameters.hpp

--- Rsc/Parameters.hpp
+++ Rsc/Parameters.hpp
@@ -275,8 +275,8 @@ class Params {
 
 	class CTI_PLAYERS_GROUPSIZE {
 		title = $STR_CTI_PLAYERS_GROUPSIZE;
-		values[] = {0,1,2,3,4,5,8,10,12,14,16};
-		texts[] = {$STR_CTI_PLAYERS_GROUPSIZE_AUTO,"1","2","3","4","5","8","10","12","14","16"};
+		values[] = {-10,0,1,2,3,4,5,8,10,12,14,16};
+		texts[] = {$STR_CTI_DISABLED, $STR_CTI_PLAYERS_GROUPSIZE_AUTO,"1","2","3","4","5","8","10","12","14","16"};
 		default = 0;
 	};
 	class CTI_GAMEPLAY_MISSILES_RANGE {
